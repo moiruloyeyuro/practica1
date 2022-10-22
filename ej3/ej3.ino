83fab8c6068cf1149004184d5e33f0b1756ec829
Moises <757277@unizar.es>
2022-10-22 15:45:58 +0200

    Update ej3.ino

--- ej3/ej3.ino
+++ ej3/ej3.ino
@@ -1,10 +1,11 @@
 //potenciometro
 const int portPin = 34; //Pin del potenciómetro
+int duty; 
 int x;
 int potValor; //valor potenciometro
 
 //pwm
-const int pwm = 13;  // GPIO13 salida pwm
+const int pwm = 33;  // GPIO33 salida pwm
 const int frecuencia = 5000;//Hz
 const int ledChannel = 0; //Canal
 const int resolucion = 8; //bits, de 0 a 255
@@ -12,16 +13,14 @@ const int resolucion = 8; //bits, de 0 a 255
 void setup(){
   Serial.begin(115200);
   // configuracion pwm
-  ledcSetup(portPin, frecuencia, resolucion); 
-  ledcAttachPin(pwm, portPin); //salida pwm
+  ledcSetup(ledChannel, frecuencia, resolucion); 
+  ledcAttachPin(pwm, ledChannel); //salida pwm
 }
  
 void loop(){
   potValor = analogRead(portPin);
   //lee potenciometro
-  x = map(potValor, 0, 4095 , 0, 255); 
-  // ajusta a 255 (resolucion)
-  ledcWrite(portPin,x);
-  delay(20);
- 
+  duty = map(potValor, 0, 4095 , 0, 255); //da un valor de duty proporcional al estado del potenciometro
+  Serial.println(duty); //Envío del valor al puerto serie
+  ledcWrite(ledChannel,duty); //salida pin y duty
 }   
