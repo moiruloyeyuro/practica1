10c1846952233a4b4942d88e11b188107e0d38ac
Moises <757277@unizar.es>
2022-10-26 10:11:00 +0200

    Update Ej5.ino

--- Ej5/Ej5.ino
+++ Ej5/Ej5.ino
@@ -1,56 +1,45 @@
+#include <MPU9250_asukiaaa.h>
 
-#include <Wire.h> //librería para conectar por I2C
-#include "MPU9250_asukiaaa.h"
-#define MPU 0x68 //Direccion I2C
-int i=0;
-const int pinSDA= 22;
-const int pinSCL= 23;
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN 21
+#define SCL_PIN 22
+#endif
+
+MPU9250_asukiaaa mySensor;
+float aX, aY, aZ;
 int cont;
-int16_t AcX;
-int16_t AcY;
-int16_t AcZ;
+int i=0;
 
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("started");
 
-void I2Cread(uint8_t Direccion, uint8_t Registro, uint8_t Nbytes)
-{
-   Wire.beginTransmission(Direccion); //direccion del esclavo
-   Wire.write(Registro); //registro con el que me comunico
-   Wire.endTransmission(false);
-   Wire.requestFrom(Direccion, Nbytes,true);//leo N bytes del esclavo
+#ifdef _ESP32_HAL_I2C_H_ // For ESP32
+  Wire.begin(SDA_PIN, SCL_PIN);
+  mySensor.setWire(&Wire);
+#endif
+  mySensor.beginAccel();
 }
 
-void I2CwriteByte(uint8_t Direccion, uint8_t Registro)
-{
-   Wire.beginTransmission(Direccion);  //Inicio comunicacion
-   Wire.write(Registro);//Accedo al registro para escribir
-   Wire.endTransmission(true);
+void loop() {
+  uint8_t sensorId;
+  int result;
+for( cont = 0; cont < 10; cont ++){
+
+  result = mySensor.accelUpdate();
+  aX = mySensor.accelX();
+  aY = mySensor.accelY();
+  aZ = mySensor.accelZ();
+  delay(100);
 }
+if( cont == 10){
+    Serial.println("accelX: " + String(aX));
+    Serial.println("accelY: " + String(aY));
+    Serial.println("accelZ: " + String(aZ));
+    Serial.println("");
+    cont=0;
+    //Serial.println("accelSqrt: " + String(aSqrt));
+  } 
 
-
-void setup(){
-  // config I2C
-  Wire.begin(pinSDA, pinSCL);
-  
-  // comm serie
-  I2CwriteByte(MPU, 0x6B);
-   Serial.begin(115200);
   }
-
-void loop(){
-  for( cont = 0; cont < 10; cont ++){
-    I2Cread(MPU, 0x6B, 6);
-    i++; 
-    AcX = Wire.read()<<8|Wire.read();
-    AcY = Wire.read()<<8|Wire.read();
-    AcZ = Wire.read()<<8|Wire.read();
-    delay(100); //muestreo cada 100ms
-  }
-
-  //mando porr UART
-  if( cont == 10){
-    
-    Serial.println(String(AcX) + "," + String(AcY)+ "," + String(AcZ)); 
-    cont = 0; // reseteo el contador
-  }
-}
-
